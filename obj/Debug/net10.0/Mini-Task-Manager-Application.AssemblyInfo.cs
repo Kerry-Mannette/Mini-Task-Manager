48105df3d674a187ed6adae3b73dc3a9fa866b0e
Kerry Mannette <kerrymannette2@gmail.com>
2026-02-10 03:12:12 -0400

    Detect SQL Server provider and generic missing-connection-string message

--- obj/Debug/net10.0/Mini-Task-Manager-Application.AssemblyInfo.cs
+++ obj/Debug/net10.0/Mini-Task-Manager-Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mini-Task-Manager-Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fac39051fb3f72d7cd52e8339ed1f8624cf8aa2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32b85c306bef5272598a582dfccfc58423cddd5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mini-Task-Manager-Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mini-Task-Manager-Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
