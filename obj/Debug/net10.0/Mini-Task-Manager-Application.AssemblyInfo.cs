a32727ec808a248e6f1835d9b9c745355bd52c7e
Kerry Mannette <kerrymannette2@gmail.com>
2026-02-10 23:20:31 -0400

    Remove Policy page and layout link (user requested)

--- obj/Debug/net10.0/Mini-Task-Manager-Application.AssemblyInfo.cs
+++ obj/Debug/net10.0/Mini-Task-Manager-Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mini-Task-Manager-Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48105df3d674a187ed6adae3b73dc3a9fa866b0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a12e699e9aaba4390b494874cd3073d0c48635ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mini-Task-Manager-Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mini-Task-Manager-Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
